c00602169efc4879e99cf22cd889ec623a7cd49c
Nithin <60174062+Nithin3008@users.noreply.github.com>
2022-06-14 15:29:53 +0530

    initial commit

--- js1.js
+++ js1.js
@@ -1,27 +1,21 @@
-var txt1=document.querySelector("#text-area1")
-var btn2=document.querySelector("#btn1")
-var txt2=document.querySelector("#text-area2")
-var url="https://api.funtranslations.com/translate/minion.json"
+var txt1 = document.querySelector("#text-area1");
+var btn2 = document.querySelector("#btn1");
+var txt2 = document.querySelector("#text-area2");
+var url = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslate(text)
-{
-    return url+"?"+"text="+text;
+function getTranslate(text) {
+  return url + "?" + "text=" + text;
 }
 
-function tr()
-{
-    var inputtex=txt1.value
-    console.log(inputtex)
-    fetch(getTranslate(inputtex))
-.then(response => response.json())
-.then(json => {
-    console.log(json.contents.translated);
-    txt2.innerText = json.contents.translated;
-})
-
-
-
+function tr() {
+  var inputtex = txt1.value;
+  console.log(inputtex);
+  fetch(getTranslate(inputtex))
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json.contents.translated);
+      txt2.innerText = json.contents.translated;
+    });
 }
 
-
-btn2.addEventListener("click",tr)
+btn2.addEventListener("click", tr);
